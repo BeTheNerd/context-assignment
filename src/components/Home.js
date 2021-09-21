5c9ab05557e3a7649c42e947aa91aadd2b42dc20
Tom Hohl <tom.hohl@gmail.com>
2021-09-21 16:37:16 -0600

    home screen effects

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Card, Segment } from 'semantic-ui-react';
+import '../stylesheets/Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Hello! Am Home page</h1>
+        <div className="home-wrapper">
+            <Segment className="title">
+                <h1>Context-assignment app</h1>
+            </Segment>
+            <Card.Group className="group-wrapper">
+                <Card className="tom-card">
+                    <Card.Content>
+                        <Card.Header>
+                            Full Control
+                        </Card.Header>
+                        <Card.Description>Experience the joys of being able to update a user</Card.Description>
+                    </Card.Content>
+                </Card>
+                <Card className="tom-card">
+                    <Card.Content>
+                        <Card.Header>
+                            Depth
+                        </Card.Header>
+                        <Card.Description>Immerse yourself in the layers of control as you edit and view a user</Card.Description>
+                    </Card.Content>
+                </Card>
+                <Card className="tom-card">
+                    <Card.Content>
+                        <Card.Header>
+                            Responsive Design
+                        </Card.Header>
+                        <Card.Description>Feel confident that making a change will bring you your results, fast.</Card.Description>
+                    </Card.Content>
+                </Card>
+            </Card.Group>
         </div>
     )
 }
